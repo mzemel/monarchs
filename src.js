dfcf1c5433616c6be0a9758d0046108096d1c715
Michael Zemel <michael.zemel@stitchfix.com>
2019-04-18 18:42:40 -0400

    Better management of mouse in/out of blocks; details looks ok sort of; broke the axis

--- src.js
+++ src.js
@@ -27,6 +27,8 @@ var detailsHeight = height / 4 * 3,
   detailsEventsY = detailsY + 3 * detailsHeightInterval + detailsMargin,
   detailsWarsY   = detailsY + 4 * detailsHeightInterval + detailsMargin;
 
+var detailsOpen = false; // yay global state variables
+
 function render(data) {
   // Flatten all reigns into a single array to determine start and end
   var firstYear = d3.min(_.flatten(_.map(data, function(countryData, countryName) { return _.map(countryData, function(monarchData, monarchName) { return monarchData.start; }); })));
@@ -37,33 +39,20 @@ function render(data) {
   // Create Timeline
   var timeline = d3.select("body")
     .append("svg")
-    .attr({
-      width: width,
-      height: height,
-      class: "timeline"
-    })
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "timeline")
 
-  // Create detail box
-  var detail = d3.select("body")
-    .append("svg")
-    .attr({
-      width: width,
-      height: height,
-      class: 'detail hidden'
-    })
+  var detail = timeline.append("rect")
+    .attr("width", 0)
+    .attr("height", 0)
+    .attr("x", width / 2)
+    .attr("y", height / 2)
+    .attr("fill", "gainsboro")
+    .attr("rx", detailsRx)
+    .attr("ry", detailsRy)
     .on("click", hideDetail)
 
-  detail.append("rect").attr({
-    width: detailsWidth,
-    height: detailsHeight,
-//    x: (width + margin.left + margin.right) / 2,
-//    y: (height + margin.bottom + margin.top - detailsHeight) / 2,
-    x: detailsX,
-    y: detailsY,
-    fill: "gainsboro",
-    rx: detailsRx,
-    ry: detailsRy
-  });
 
   // Create X-axis
   var xScale = d3.time.scale()
@@ -72,188 +61,177 @@ function render(data) {
 
   var xAxis = d3.svg.axis().scale(xScale).tickFormat(d3.format("d"));
 
-  timeline.append("g").attr({
-    "class": "axis",
-    transform: "translate(" + [0, height - margin.bottom] + ")"
-  }).call(xAxis);
+  timeline.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+    .call(xAxis);
 
   function handleMouseOver(el, i) {
+    if (detailsOpen) return false
+
     var $this = $(this),
       $x = parseFloat($this.attr('x')),
       $y = parseFloat($this.attr('y')),
       $width = $this.width(),
       $height = $this.height();
     
-    d3.select(this).transition().attr({
-      "fill-opacity": 1.0,
-    })
-
-    // enlargeBlock(this);
+    enlargeBlock.bind(d3.select(this))(this);
 
     var thumbnailImageWidth = width / 8;
 
-    timeline.append("image").transition().attr({
-      x: $x + $width / 2 - thumbnailImageWidth / 2,
-      y: $y - thumbnailImageWidth - detailsLineHeight,
-      width: thumbnailImageWidth,
-      height: thumbnailImageWidth,
-      "xlink:href": el.image,
-      class: "thumbnail"
-    }).delay(0)
-
-    timeline.append("text").transition().attr({
-      x: $x + $width / 2,
-      y: $y - thumbnailImageWidth - detailsLineHeight * 2,
-      "text-anchor": "middle",
-      class: "thumbnail"
-    }).delay(0).text(el.name);
+    timeline.append("image")
+      .attr("x", $x + $width / 2)
+      .attr("y", $y + laneHeight / 2)
+      .transition()
+      .attr("x", $x + $width / 2 - thumbnailImageWidth / 2)
+      .attr("y", $y - thumbnailImageWidth - detailsLineHeight)
+      .attr("width", thumbnailImageWidth)
+      .attr("height", thumbnailImageWidth)
+      .attr("xlink:href", el.image)
+      .attr("class", "thumbnail")
   };
 
   function handleMouseOut(el, i) {
-    d3.selectAll(".thumbnail").remove();
-    d3.select(this).transition().attr({
-      "fill-opacity": 0.5,
-    });
-    // reduceBlock(this);
+    if (detailsOpen) return false
+
+    timeline.selectAll(".thumbnail").remove();
+    reduceBlock.bind(d3.select(this))(this);
   };
 
-  function enlargeBlock(rect) {
-    var $this = $(rect),
+  function enlargeBlock(el) {
+    var $this = $(el),
       $x = parseFloat($this.attr('x')),
       $y = parseFloat($this.attr('y')),
       $width = $this.width(),
       $height = $this.height();
 
-    d3.select(this).transition().attr({
-      x: $x - 5,
-      y: $y - 5,
-      width: $width + 10,
-      height: $height + 10,
-      "prevX": $x,
-      "prevY": $y,
-      "prevWidth": $width,
-      "prevHeight": $height,
-    })
+    this
+      .attr("x", $x - 5)
+      .attr("y", $y - 5)
+      .attr("width", $width + 10)
+      .attr("height", $height + 10)
+      .attr("smallX", this.attr("smallX") || $x)
+      .attr("smallY", this.attr("smallY") || $y)
+      .attr("smallWidth", this.attr("smallWidth") || $width)
+      .attr("smallHeight", this.attr("smallHeight") || $height)
+      .attr("fill-opacity", 1.0);
   }
 
-  function reduceBlock(rect) {
-    var $this = $(rect);
-    if ($this.attr('prevX') !== null) {
-      var $x = parseFloat($this.attr('x')),
-        $y = parseFloat($this.attr('y')),
-        $width = $this.width(),
-        $height = $this.height();
+  function reduceBlock(el) {
+    var $this = $(el);
+    var $x = parseFloat($this.attr('x')),
+      $y = parseFloat($this.attr('y')),
+      $width = $this.width(),
+      $height = $this.height();
 
-      var rect = d3.select(rect);
-
-      d3.select(this).transition().attr({
-        "fill-opacity": 0.5,
-        x: rect.attr("prevX"),
-        y: rect.attr('prevY'),
-        width: rect.attr('prevWidth'),
-        height: rect.attr('prevHeight'),
-        "prevX": null,
-        "prevY": null,
-        "prevWidth": null,
-        "prevHeight": null
-      });
-    }
+    this
+      .attr("fill-opacity", 0.5)
+      .attr("x", this.attr("smallX"))
+      .attr("y", this.attr('smallY'))
+      .attr("width", this.attr('smallWidth'))
+      .attr("height", this.attr('smallHeight'))
+      .attr("fill-opacity", 0.5)
   }
 
   function showDetail(el, i) {
-    timeline.attr({class: 'timeline inactive'});
-    detail.attr({class: 'detail'})
-    renderDetails(el);
+    timeline.classed('inactive', true);
+    detail.transition()
+      .attr("width", detailsWidth)
+      .attr("height", detailsHeight)
+      .attr("x", detailsX)
+      .attr("y", detailsY)
+      .duration(300).on("end", function() { renderDetails(el); });
+    handleMouseOut = handleMouseOut.bind(this);
+    handleMouseOut(el, i);
+    detailsOpen = true;
   }
 
-  function hideDetail() {
-    timeline.attr({class: 'timeline'});
-    detail.attr({class: 'detail hidden'});
-    detail.selectAll("text").remove();
-    detail.selectAll("image").remove();
-
-    detail.select(".details-name").remove();
+  function hideDetail(el, i) {
+    timeline.classed('inactive', false);
+    detailsOpen = false;
+    detail.transition()
+      .attr("width", 0)
+      .attr("height", 0)
+      .attr("x", width / 2)
+      .attr("y", height / 2)
+    timeline.selectAll('.detail').remove();
   }
 
   function renderDetails(el) {
     // Monarch
-    detail.append("image").attr({
-      x: detailsImageX,
-      y: detailsImageY,
-      width: detailsImageWidth,
-      height: detailsImageHeight,
-      "xlink:href": el.image,
-      preserveAspectRatio: "none"
-    })
+    timeline.append("image")
+      .attr("x", detailsImageX)
+      .attr("y", detailsImageY)
+      .attr("width", detailsImageWidth)
+      .attr("height", detailsImageHeight)
+      .attr("xlink:href", el.image)
+      .attr("preserveAspectRatio", "none")
+      .attr("class", "detail")
 
     // House
-    detail.append("image").attr({
-      x: detailsX + detailsWidth - detailsImageWidth - detailsWidthInterval,
-      y: detailsImageY,
-      width: detailsImageWidth,
-      height: detailsImageHeight,
-      "xlink:href": el.houseImage,
-      preserveAspectRatio: "none"
-    })
+    detail.append("image")
+      .attr("x", detailsX + detailsWidth - detailsImageWidth - detailsWidthInterval)
+      .attr("y", detailsImageY)
+      .attr("width", detailsImageWidth)
+      .attr("height", detailsImageHeight)
+      .attr("xlink:href", el.houseImage)
+      .attr("preserveAspectRatio", "none")
+      .attr("class", "detail")
 
     // Details: Name, House, Reign
-//    detail.append("rect").attr({
-//      x: detailsImageX,
-//      y: detailsNameY,
-//      width: detailsWidth - 2 * detailsWidthInterval,
-//      height: detailsHeightInterval,
-//      "fill-opacity": 0.2,
-//      class: "details-name"
-//    })
-    detail.append("text").attr({
-      x: detailsImageX + detailsImageWidth / 2,
-      y: detailsNameY + detailsLineHeight,
-      "text-anchor": "middle",
-//      textLength: detailsImageWidth,
-//      lengthAdjust: "spacesAndGlyphs"
-    }).text(el.name);
-    detail.append("text").attr({
-      x: detailsX + detailsWidth - detailsImageWidth / 2 - detailsWidthInterval,
-      y: detailsNameY + detailsLineHeight,
-      "text-anchor": "middle",
-//      textLength: detailsImageWidth,
-//      lengthAdjust: "spacesAndGlyphs"
-    }).text(el.house);
-    detail.append("text").attr({
-      x: detailsMiddle,
-      y: detailsNameY + 2 * detailsLineHeight,
-      "text-anchor": "middle"
-    }).text(el.start + "-" + el.end + " (" + el.endReason + ")");
-    detail.append("text").attr({
-      x: detailsMiddle,
-      y: detailsNameY + 3 * detailsLineHeight,
-      "text-anchor": "middle"
-    }).text(el.religion);
+    detail.append("text")
+      .attr("x", detailsImageX + detailsImageWidth / 2)
+      .attr("y", detailsNameY + detailsLineHeight)
+      .attr("text-anchor", "middle")
+      .attr("class", "detail")
+      .text(el.name);
+    detail.append("text")
+      .attr("x", detailsX + detailsWidth - detailsImageWidth / 2 - detailsWidthInterval)
+      .attr("y", detailsNameY + detailsLineHeight)
+      .attr("text-anchor", "middle")
+      .attr("class", "detail")
+      .text(el.house);
+    detail.append("text")
+      .attr("x", detailsMiddle)
+      .attr("y", detailsNameY + 2 * detailsLineHeight)
+      .attr("text-anchor", "middle")
+      .attr("class", "detail")
+      .text(el.start + "-" + el.end + " (" + el.endReason + ")");
+    detail.append("text")
+      .attr("x", detailsMiddle)
+      .attr("y", detailsNameY + 3 * detailsLineHeight)
+      .attr("text-anchor", "middle")
+      .attr("class", "detail")
+      .text(el.religion);
 
     // Events
-    detail.append("text").attr({
-      x: detailsMiddle,
-      y: detailsEventsY + detailsLineHeight,
-      "text-anchor": "middle"
-    }).text("Events")
+    detail.append("text")
+      .attr("x", detailsMiddle)
+      .attr("y", detailsEventsY + detailsLineHeight)
+      .attr("text-anchor", "middle")
+      .attr("class", "detail")
+      .text("Events")
     _.map(el.events, function(event, idx) {
-      detail.append("text").attr({
-        x: detailsImageX,
-        y: detailsEventsY + detailsLineHeight * (2 + idx)
-      }).text(event);
+      detail.append("text")
+        .attr("x", detailsImageX)
+        .attr("y", detailsEventsY + detailsLineHeight * (2 + idx))
+        .attr("class", "detail")
+        .text(event);
     })
 
     // Wars
-    detail.append("text").attr({
-      x: detailsMiddle,
-      y: detailsWarsY + detailsLineHeight,
-      "text-anchor": "middle"
-    }).text("Wars")
+    detail.append("text")
+      .attr("x", detailsMiddle)
+      .attr("y", detailsWarsY + detailsLineHeight)
+      .attr("text-anchor", "middle")
+      .attr("class", "detail")
+      .text("Wars")
     _.map(el.wars, function(war, idx) {
-      detail.append("text").attr({
-        x: detailsImageX,
-        y: detailsWarsY + detailsLineHeight * (2 + idx)
-      }).text(war);
+      detail.append("text")
+        .attr("x", detailsImageX)
+        .attr("y", detailsWarsY + detailsLineHeight * (2 + idx))
+        .attr("class", "detail")
+        .text(war);
     })
 
     
@@ -273,19 +251,19 @@ function render(data) {
     _.map(el.relationships, function(rel, idx) {
       var relationshipImageX = detailsMiddle - (relationshipCount - 1) * 0.75 * relationshipImageWidth + idx * 1.5 * relationshipImageWidth - 0.5 * relationshipImageWidth,
         relationshipImageY = detailsY + 5 * detailsHeightInterval + detailsMargin;
-      detail.append("image").attr({
-        x: relationshipImageX,
-        y: relationshipImageY,
-        width: relationshipImageWidth,
-        height: relationshipImageWidth,
-        "xlink:href": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Nicolas_Cage_2011_CC.jpg/220px-Nicolas_Cage_2011_CC.jpg"
-      })
+      detail.append("image")
+        .attr("x", relationshipImageX)
+        .attr("y", relationshipImageY)
+        .attr("width", relationshipImageWidth)
+        .attr("height", relationshipImageWidth)
+        .attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Nicolas_Cage_2011_CC.jpg/220px-Nicolas_Cage_2011_CC.jpg")
+
       _.map(rel.split(","), function(relComponent, idx) {
-        detail.append("text").attr({
-          x: relationshipImageX + relationshipImageWidth / 2,
-          y: relationshipImageY + relationshipImageWidth + detailsLineHeight * (idx + 1),
-          "text-anchor": "middle",
-        }).text(relComponent);
+        detail.append("text")
+          .attr("x", relationshipImageX + relationshipImageWidth / 2)
+          .attr("y", relationshipImageY + relationshipImageWidth + detailsLineHeight * (idx + 1))
+          .attr("text-anchor", "middle")
+          .text(relComponent);
       });
     });
   }
@@ -301,26 +279,25 @@ function formatDetails(el) {
     countryIndex += 1;
 
     // Add legend
-    timeline.append("text").attr({
-      x: 0,
-      y: height - margin.bottom - laneHeight * (countryIndex - 1) - lanePadding * countryIndex
-    }).text(countryName);
+    timeline.append("text")
+      .attr("x", 0)
+      .attr("y", height - margin.bottom - laneHeight * (countryIndex - 1) - lanePadding * countryIndex)
+      .text(countryName);
 
     // Add elements
-    timeline.append("g").attr({id: countryName})
+    timeline.append("g")
+      .attr('id', countryName)
       .selectAll("rect")
       .data(_.map(countryData, function(monarchData, monarchName) { return monarchData; }))
       .enter()
       .append("rect")
-      .attr({
-        width: function(el) { return (el.end - el.start) * pixelsPerYear - 1 }, // 1 px padding
-        height: laneHeight,
-        x: function(el) { return margin.left + (el.start - firstYear) * pixelsPerYear },
-        y: height - margin.bottom - laneHeight * (countryIndex - 1) - lanePadding * countryIndex,
-        fill: "blue",
-        "fill-opacity": 0.5,
-        class: "block"
-      })
+      .attr("width", function(el) { return (el.end - el.start) * pixelsPerYear - 1 })
+      .attr("height", laneHeight)
+      .attr("x", function(el) { return margin.left + (el.start - firstYear) * pixelsPerYear })
+      .attr("y", height - margin.bottom - laneHeight * (countryIndex - 1) - lanePadding * countryIndex)
+      .attr("fill", "blue")
+      .attr("fill-opacity", 0.5)
+      .attr("class", "block")
       .on("mouseover", handleMouseOver)
       .on("mouseout", handleMouseOut)
       .on("click", showDetail)
